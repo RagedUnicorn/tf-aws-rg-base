86226b03483ac28eb240919065d3b4f52785676e
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2018-07-28 15:57:09 +0200

    Fix output variables for ragedunicorn_base

--- ragedunicorn_base/output.tf
+++ ragedunicorn_base/output.tf
@@ -3,20 +3,20 @@
 #####
 output "id" {
   description = "ID of the created bucket"
-  value       = "${aws_s3_bucket.state_store.id}"
+  value       = "${aws_s3_bucket.ragedunicorn_backend.id}"
 }
 
 output "arn" {
   description = "The ARN of the bucket."
-  value       = "${aws_iam_user.state_store.arn}"
+  value       = "${aws_s3_bucket.ragedunicorn_backend.arn}"
 }
 
 output "bucket_domain_name" {
   description = " The bucket domain name."
-  value       = "${aws_s3_bucket.state_store.bucket_domain_name}"
+  value       = "${aws_s3_bucket.ragedunicorn_backend.bucket_domain_name}"
 }
 
 output "region" {
   description = "The AWS region this bucket resides in."
-  value       = "${aws_s3_bucket.state_store.region}"
+  value       = "${aws_s3_bucket.ragedunicorn_backend.region}"
 }
