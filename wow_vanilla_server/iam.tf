7c680939586dfd01682aae3abd94cbb4c5ba7d4e
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2018-07-28 22:55:13 +0200

    Update policy documents for vpc and elastic ip setup

--- wow_vanilla_server/iam.tf
+++ wow_vanilla_server/iam.tf
@@ -8,6 +8,7 @@ data "aws_iam_policy_document" "iam_policy_document" {
       "iam:PassRole",
       "iam:GetRolePolicy",
       "iam:GetRole",
+      "iam:GetUser",
       "iam:CreateRole",
       "iam:DeleteRole",
       "iam:DeleteRolePolicy",
@@ -75,6 +76,7 @@ data "aws_iam_policy_document" "lambda_policy_document" {
       "lambda:AddPermission",
       "lambda:RemovePermission",
       "lambda:DeleteFunction",
+      "lambda:UpdateFunctionConfiguration",
     ]
 
     resources = [
@@ -105,44 +107,27 @@ resource "aws_iam_user_policy" "lambda_policy" {
 data "aws_iam_policy_document" "ec2_policy_document" {
   statement {
     actions = [
+      "ec2:Describe*",
+      "ec2:CreateSecurityGroup",
+      "ec2:CreateSecurityGroup",
+      "ec2:CreateVpc",
+      "ec2:ModifyVpcAttribute",
+      "ec2:CreateInternetGateway",
       "ec2:DeleteSecurityGroup",
-      "ec2:AuthorizeSecurityGroupEgress",
-      "ec2:AuthorizeSecurityGroupIngress",
-      "ec2:RevokeSecurityGroupIngress",
-      "ec2:RevokeSecurityGroupEgress",
+      "ec2:AuthorizeSecurityGroup*", # AuthorizeSecurityGroup Ingress / Egress
+      "ec2:RevokeSecurityGroup*", # RevokeSecurityGroup Ingress / Egress
       "ec2:RunInstances",
-      "ec2:StopInstances",
-      "ec2:StartInstances",
+      "ec2:Stop*", # StopInstances
+      "ec2:Start*", # StartInstances
       "ec2:TerminateInstances",
       "ec2:CreateTags",
       "ec2:CreateVolume",
       "ec2:AttachVolume",
-    ]
-
-    resources = [
-      "arn:aws:ec2:*:*:subnet/*",
-      "arn:aws:ec2:*:*:instance/*",
-      "arn:aws:ec2:*:*:volume/*",
-      "arn:aws:ec2:*:*:security-group/*",
-      "arn:aws:ec2:*:*:network-interface/*",
-      "arn:aws:ec2:*::image/*",
-      "arn:aws:ec2:*:*:*",
-    ]
-  }
-
-  statement {
-    actions = [
-      "ec2:DescribeInstanceCreditSpecifications",
-      "ec2:DescribeAccountAttributes",
-      "ec2:DescribeInstanceAttribute",
-      "ec2:DescribeNetworkInterfaces",
-      "ec2:DescribeVolumes",
-      "ec2:DescribeTags",
-      "ec2:DescribeKeyPairs",
-      "ec2:DescribeInstances",
-      "ec2:DescribeImages",
-      "ec2:DescribeSecurityGroups",
-      "ec2:CreateSecurityGroup",
+      "ec2:AttachInternetGateway",
+      "ec2:CreateSubnet",
+      "ec2:ModifySubnetAttribute",
+      "ec2:AllocateAddress",
+      "ec2:AssociateAddress",
     ]
 
     resources = [
