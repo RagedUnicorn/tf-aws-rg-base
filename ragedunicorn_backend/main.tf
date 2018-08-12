17029c2aaaab61d9b1a46f1936230f8fe84e3e8f
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2018-08-13 01:46:56 +0200

    Lock terraform provider versions

--- ragedunicorn_backend/main.tf
+++ ragedunicorn_backend/main.tf
@@ -2,6 +2,7 @@
 # AWS provider
 ###############
 provider "aws" {
+  version    = "~> 1.31"
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   region     = "${var.aws_region}"
--- wow_vanilla_server/main.tf
+++ wow_vanilla_server/main.tf
@@ -10,6 +10,7 @@ terraform {
 # AWS provider
 ###############
 provider "aws" {
+  version    = "~> 1.31"
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   region     = "${var.aws_region}"
